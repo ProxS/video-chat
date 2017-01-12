afd36b74cf1b0452f4e12739844a748e120b3ca0
ProxS <sergey_proskuryakov_93@mail.ru>
2017-01-12 12:37:01 +0300

    add

--- server.js
+++ server.js
@@ -8,44 +8,18 @@ var app = http.createServer(function (req, res) {
 var io = require('socket.io').listen(app);
 
 io.sockets.on('connection', function (socket) {
-//	function log() {
-//		var array = [">>> "];
-//		for(var i = 0; i < arguments.length; i++) {
-//			array.push(arguments[i]);
-//		}
-//		socket.emit('log', array);
-//	}
 
 	socket.on('message', function (message) {
 		console.log('Got message: ' + message);
 		socket.broadcast.emit('message', message);
 	});
 
-	socket.on('create or join', function (room) {
-		var numClients = io.sockets.clients(room).length;
+	socket.on('speak_room', function (room) {
 
-		console.log('Room ' + room + ' has ' + numClients + ' client(s)');
-		console.log('Request to create or join room', room);
-
-		if(numClients == 0) {
-			socket.join(room);
-			socket.emit('created', room);
-		} 
-
-		else if(numClients == 1) {
-			io.sockets.in(room).emit('join', room);
-			socket.join(room);
-			socket.emit('joined', room);
-		} 
-
-		else {
-			socket.emit('full', room);
-		}
-
-		socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
-		socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
+        
+        socket.emit('speak_room', room);
+        
 	});
-    
     socket.on('chat message', function(msg){
         var name = 'Socket-' + socket.id;
         io.emit('chat message', msg);
